1d85fb000000f12e84f326dd69bad4345dfd972a
Jan <janlukasschroeder@protonmail.com>
2018-12-03 07:48:09 +1100

    Cleaned up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Table from "./components/Table";
-import Margin from "./components/Margin";
-import FullscreenScroll from "./components/FullscreenScroll";
-import Dropdown from "./components/Dropdown";
 import DragAndDrop from "./components/DragAndDrop";
 
 import "./styles.css";
